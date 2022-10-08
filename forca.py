1ddce5049cf797361e89f2f1ed31587a92ff9f9f
Cesar Carvalho <104288152+Cesarksc@users.noreply.github.com>
2022-10-08 15:42:59 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,22 +1,54 @@
+
 def jogar():
+    import random
+
     print ('********************************')
     print ('Bem vindo! ao jogo de adivinhação')
     print ('********************************')
 
-    palavra_secreta = 'Banana'
+    arquivo = open('Frutas.txt', 'r')
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].lower()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     #enquanto não perdeu e/ou ganhou o jogador irá continuar tentando
     while(not enforcou and not acertou):
         chute = input('Qual letra? ')
-        chute = chute.strip()
+        chute = chute.strip().lower()
 
-        index = 1
-        for letra in palavra_secreta:
-            if (chute.lower() == letra.lower()):
-             print('Encontrada a letra {} na posição {}'.format(chute, index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+
+        if acertou:
+            print('Você acertou a palavra secreta')
+        else:
+            print('Você perdeu')
+
 
     print('Fim do jogo')
 
