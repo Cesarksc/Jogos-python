cf2d45cd32e76d1c4b6526e42858c11b0739e7ff
Cesar Carvalho <104288152+Cesarksc@users.noreply.github.com>
2022-10-08 00:30:56 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,6 +3,20 @@ def jogar():
     print ('Bem vindo! ao jogo de adivinhação')
     print ('********************************')
 
+    palavra_secreta = 'Banana'
+    enforcou = False
+    acertou = False
+
+    #enquanto não perdeu e/ou ganhou o jogador irá continuar tentando
+    while(not enforcou and not acertou):
+        chute = input('Qual letra? ')
+        chute = chute.strip()
+
+        index = 1
+        for letra in palavra_secreta:
+            if (chute.lower() == letra.lower()):
+             print('Encontrada a letra {} na posição {}'.format(chute, index))
+            index = index + 1
 
     print('Fim do jogo')
 
